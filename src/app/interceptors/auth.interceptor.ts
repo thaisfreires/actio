38a9dce633dc53c3fcdd476444e70f367d2820d6
Thais <thaisfrsoares@gmail.com>
2025-07-07 14:47:10 +0100

    fix register endpoint-add validation verification

--- src/app/interceptors/auth.interceptor.ts
+++ src/app/interceptors/auth.interceptor.ts
@@ -3,7 +3,14 @@ import { HttpInterceptorFn } from '@angular/common/http';
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
   const token = localStorage.getItem('token');
 
-  if (token) {
+  const publicUrls = [
+    '/login',
+    '/users/save'
+  ];
+  console.log('token', token);
+  const isPublic = publicUrls.some(url => req.url.includes(url));
+
+  if (token && !isPublic) {
     const authReq = req.clone({
       headers: req.headers.set('Authorization', `Bearer ${token}`)
       });
