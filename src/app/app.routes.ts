e17a8eec6efe1a798b78ae4aa537fe538a68e831
Priscila C <priscila.campoos@yahoo.com.br>
2025-07-13 21:47:22 +0100

    create page unauthorized and add it to routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
+import { UnauthorizedPageComponent } from './pages/unauthorized-page/unauthorized-page.component';
 
 export const routes: Routes = [
     {path: 'login', pathMatch: 'full', component: LoginComponent},
-    { path: 'register', component: RegisterComponent }
+    { path: 'register', component: RegisterComponent },
+    { path: 'unauthorized', component: UnauthorizedPageComponent }
 ];
