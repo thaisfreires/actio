8d46bba9d444e2ae66b701953a0d68971e50d214
Priscila C <priscila.campoos@yahoo.com.br>
2025-07-14 12:56:50 +0100

    fix merge conflicts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
 import { UnauthorizedPageComponent } from './pages/unauthorized-page/unauthorized-page.component';
 import { ProfileComponent } from './pages/profile/profile.component';
+import { roleGuard } from './auth/role.guard';
 
 export const routes: Routes = [
     {
@@ -22,11 +23,11 @@ export const routes: Routes = [
     {
       path: 'unauthorized',
       component: UnauthorizedPageComponent
-    }
+    },
     {
       path: 'profile',
-      component: ProfileComponent
-      canActivate: [RoleGuard],
+      component: ProfileComponent,
+      canActivate: [roleGuard],
       data: { roles: ['CLIENT', 'ADMIN'] }
     },
 ];
