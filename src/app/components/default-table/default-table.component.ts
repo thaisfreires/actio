b7f5955a4a35902e64bb7426ff07edffd845b5db
Priscila C <priscila.campoos@yahoo.com.br>
2025-07-15 20:55:00 +0100

    update default-table component

--- src/app/components/default-table/default-table.component.ts
+++ src/app/components/default-table/default-table.component.ts
@@ -10,52 +10,19 @@ import { CommonModule } from '@angular/common';
 })
 export class DefaultTableComponent {
 
-
   @Input() tableTitle: String = "";
   @Input() tableSubTitle: String = "";
   @Input() tableData: Array<Record<string, any>> = [];
-  @Input() tableColumns: Array<{
-    key: string;
-    label: string;
-    align?: 'start' | 'center' | 'end';
-    format?: (value: any, row?: any) => string;
-  }> = [];
-
-  /*
-   stockItems: StockItem[] = [
-    {
-      stockName: "GALP.LS",
-      quantity: 120,
-      currentValue: 13.4565,
-      dailyVariation: "+1.52%"
-    },
-    {
-      stockName: "EDP.LS",
-      quantity: 80,
-      currentValue: 4.82,
-      dailyVariation: "-0.34%"
-    },
-    {
-      stockName: "SAN.PA",
-      quantity: 45,
-      currentValue: 88.67,
-      dailyVariation: "+0.78%"
-    },
-    {
-      stockName: "OR.PA",
-      quantity: 25,
-      currentValue: 430.10,
-      dailyVariation: "+1.05%"
-    },
-    {
-      stockName: "BNP.PA",
-      quantity: 60,
-      currentValue: 56.20,
-      dailyVariation: "-0.12%"
-    }
-  ];
-  */
+  @Input() tableColumns: TableColumn[] = [];
 
   constructor() {}
 
+}
+
+export interface TableColumn {
+  label: string;
+  key?: string;
+  align?: 'start' | 'center' | 'end';
+  format?: (value: any, row?: any) => string;
+  customRender?: (row: any) => any;
 }
