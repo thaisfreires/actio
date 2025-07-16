02b9c0ce00123ac516793155b5953ea4054bf556
Priscila C <priscila.campoos@yahoo.com.br>
2025-07-16 13:18:20 +0100

    implement pagination on default-table

--- src/app/components/default-table/default-table.component.ts
+++ src/app/components/default-table/default-table.component.ts
@@ -1,22 +1,48 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
-@Component({
-  selector: 'app-default-table',
-  imports: [CommonModule],
-  templateUrl: './default-table.component.html',
-  styleUrl: './default-table.component.scss',
-})
-export class DefaultTableComponent {
-  @Input() tableTitle: String = '';
-  @Input() tableSubTitle: String = '';
-  @Input() tableData: Array<Record<string, any>> = [];
-  @Input() tableColumns: TableColumn[] = [];
-  constructor() {}
-}
+
 export interface TableColumn {
   label: string;
   key?: string;
   align?: 'start' | 'center' | 'end';
   format?: (value: any, row?: any) => string;
   customRender?: (row: any) => any;
+}
+
+@Component({
+  selector: 'app-default-table',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './default-table.component.html',
+  styleUrls: ['./default-table.component.scss']
+})
+export class DefaultTableComponent {
+  @Input() tableData: Record<string, any>[] = [];
+  @Input() tableColumns: TableColumn[] = [];
+  @Input() tableTitle = '';
+  @Input() tableSubTitle = '';
+
+  currentPage = 1;
+  readonly pageSize = 5;
+
+  get totalPages(): number {
+    return Math.max(Math.ceil(this.tableData.length / this.pageSize), 1);
+  }
+
+  get displayData(): Record<string, any>[] {
+    const start = (this.currentPage - 1) * this.pageSize;
+    return this.tableData.slice(start, start + this.pageSize);
+  }
+
+  goToPreviousPage(): void {
+    if (this.currentPage > 1) {
+      this.currentPage--;
+    }
+  }
+
+  goToNextPage(): void {
+    if (this.currentPage < this.totalPages) {
+      this.currentPage++;
+    }
+  }
 }
