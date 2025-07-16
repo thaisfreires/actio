456072571c840f79057629bc2dd0f6597adfe0f5
Priscila C <priscila.campoos@yahoo.com.br>
2025-07-16 19:35:12 +0100

    update transaction  service

---
+++ src/app/services/transaction.service.ts
@@ -0,0 +1,37 @@
+import { TransactionRequest, TransactionResponse } from './../models/transaction.model';
+import { Injectable } from '@angular/core';
+import { catchError, Observable, throwError } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TransactionService {
+
+  private bseUrl = 'http://localhost:8080/transactions';
+
+  constructor(private http: HttpClient) { }
+
+  buy(request: TransactionRequest): Observable<TransactionResponse> {
+    return this.doTransaction(request, "buy");
+  }
+
+  sell(request: TransactionRequest): Observable<TransactionResponse> {
+    return this.doTransaction(request, "sell");
+  }
+
+  private doTransaction(transaction: TransactionRequest, endpoint: string): Observable<TransactionResponse> {
+    const url = `${this.bseUrl}/${endpoint}`;
+    return this.http.post<TransactionResponse>(url, transaction).pipe(
+      catchError(error => {
+        if (error.status === 400 && typeof error.error === 'string') {
+          console.warn('Bad Request:', error.error);
+          return throwError(() => new Error(error.error));
+        }
+
+        console.error('Transaction failed:', error);
+        return throwError(() => new Error('Transaction failed. Please try again.'));
+      })
+    );
+  }
+}
