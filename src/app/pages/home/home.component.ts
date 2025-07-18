9d53124a2c393b55595384a51daa4905261204de
Thais <thaisfrsoares@gmail.com>
2025-07-18 18:21:48 +0100

    Add images to cards

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -7,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
-
+  image1 = 'assets/images/coin.png';
+  image2 = 'assets/images/investment.png';
+  image3 = 'assets/images/trade_2.png';
+  image4 = 'assets/images/lock.png';
+  image5 = 'assets/images/trade.png';
+  image6 = 'assets/images/bank.png';
+  image7 = 'assets/images/invest_2.png';
+  image8 = 'assets/images/account_2.png';
+  image9 = 'assets/images/account.png';
+  image10 = 'assets/images/consult.png';
 }
