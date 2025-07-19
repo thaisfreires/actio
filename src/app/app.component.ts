c90b470069b6fa18c6744a5dcc598f878204c17c
Thais <thaisfrsoares@gmail.com>
2025-07-19 17:59:39 +0100

    Redefine app layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,22 +6,34 @@ import { FooterComponent } from "./components/footer/footer.component";
 import { SidebarComponent } from "./components/sidebar/sidebar.component";
 import { filter } from 'rxjs/operators';
 import { AuthService } from './services/auth.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, FormsModule, NavbarComponent, FooterComponent, SidebarComponent],
+  imports: [RouterOutlet, FormsModule, NavbarComponent, FooterComponent, SidebarComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+  sidebarOpen: boolean = false;
   showLayout = true;
 
-  constructor(private router: Router) {
+  constructor(private router: Router, private authService: AuthService) {
     this.router.events.pipe(
       filter(event => event instanceof NavigationEnd)
     ).subscribe((event: any) => {
       const currentUrl = event.url;
       this.showLayout = !(currentUrl.includes('/login') || currentUrl.includes('/register'));
     });
+  }
+  toggleSidebar(): void {
+    this.sidebarOpen = !this.sidebarOpen;
+  }
+
+  closeSidebar(): void {
+    this.sidebarOpen = false;
+  }
+  get isLoggedIn(): boolean {
+    return this.authService.isLoggedIn();
   }
 }
