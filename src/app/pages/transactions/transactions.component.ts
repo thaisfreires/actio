7042dec55c914f45532b85347b4f16138b523cea
pris-c <priscila.campoos@yahoo.com.br>
2025-08-08 17:28:23 +0100

    update transaction component

--- src/app/pages/transactions/transactions.component.ts
+++ src/app/pages/transactions/transactions.component.ts
@@ -4,27 +4,26 @@ import { TransactionResponse } from '../../models/transaction.model';
 import { DefaultTableComponent, TableColumn } from '../../components/default-table/default-table.component';
 import { WalletService } from '../../services/wallet.service';
 import { CommonModule } from '@angular/common';
-
 @Component({
   selector: 'app-transactions',
   imports: [DefaultTableComponent, CommonModule],
   templateUrl: './transactions.component.html',
-  styleUrl: './transactions.component.scss'
+  styleUrls: ['./transactions.component.scss']  // plural
 })
-export class TransactionsComponent implements OnInit{
+
+export class TransactionsComponent implements OnInit {
   transactions: TransactionResponse[] = [];
   tableData: any[] = [];
-  stockNameMap = new Map<number, string>();
 
-  tableColumns: TableColumn[]= [
+  tableColumns: TableColumn[] = [
     {
-      key: 'dateTime',
+      key: 'transactionDateTime', // ajustado aqui
       label: 'Date',
       align: 'center',
-      format: (value: string) => new Date(value).toLocaleString()
+      format: (val: string) => new Date(val).toLocaleString()
     },
     {
-      key: 'stockName',
+      key: 'stockSymbol',
       label: 'Stock',
       align: 'start',
     },
@@ -37,13 +36,13 @@ export class TransactionsComponent implements OnInit{
       key: 'value',
       label: 'Price',
       align: 'end',
-      format: (val: number) => `$${val.toFixed(2)}`
+      format: (val?: number) => val != null ? `$${val.toFixed(2)}` : ''
     },
     {
       key: 'totalValue',
       label: 'Total',
       align: 'end',
-      format: (val: number) => `$${val.toFixed(2)}`
+      format: (val?: number) => val != null ? `$${val.toFixed(2)}` : ''
     },
     {
       key: 'transactionType',
@@ -54,22 +53,16 @@ export class TransactionsComponent implements OnInit{
     }
   ];
 
-  constructor(private transactionService: TransactionService, private walletService: WalletService) {}
+  constructor(private transactionService: TransactionService) {}
 
   ngOnInit(): void {
-    this.walletService.getWallet().subscribe(wallet => {
-      wallet.forEach(item => {
-        this.stockNameMap.set(item.stockId, item.stockName);
-      });
-
-      this.transactionService.getAll().subscribe(transactions => {
-        this.transactions = transactions;
-
-        this.tableData = transactions.map(tx => ({
-          ...tx,
-          stockName: this.stockNameMap.get(tx.stockId) || `#${tx.stockId}`,
-        }));
-      });
-    });
-  }
+  this.transactionService.getAll().subscribe(transactions => {
+    this.transactions = transactions;
+    this.tableData = transactions.map(tx => ({
+      ...tx,
+      stockName: tx.stockSymbol,
+      totalValue: (tx.value ?? 0) * (tx.quantity ?? 0)
+    }));
+  });
+}
 }
